9c37f6c06fb0e33bea39fd74fe4f7d8502a92241
GeekNekoS <anastasiyanesterenko@icloud.com>
2024-01-16 18:16:17 +0500

    Notes and test tasks

---
+++ java_script/hello_world.js
@@ -0,0 +1,2 @@
+const hello_world = "Hello, world!";
+console.log(hello_world)
---
+++ java_script/sum.js
@@ -0,0 +1,8 @@
+function testSum(a, b) {
+    let x;
+    x = a + b;
+    return x;
+}
+
+const ans = testSum(10, 5)
+console.log(ans)
