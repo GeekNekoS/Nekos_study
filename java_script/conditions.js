efef6cb1a12d10933277ce90e718826fa17f0ba3
GeekNekoS <anastasiyanesterenko@icloud.com>
2024-01-17 19:51:55 +0500

    The beginning of learning loops in Java Script

--- java_script/conditions.js
+++ java_script/conditions.js
@@ -34,3 +34,50 @@ function testIf(a, b) {
 }
 // answer = testIf(2, 4)
 // console.log(answer)
+
+
+function testSwitch() {
+    switch(3) {
+        case 1:
+            // блок кода 1;
+            return "case 1";
+        case 2:
+            // блок кода 2;
+            return "case 2";
+        default:
+            // блок кода если не подошло ни одно условие;
+            return "default";
+    }
+}
+// answer = testSwitch()
+// console.log(answer)
+
+
+function testSwitchByNum(a) {
+    switch(a) {
+        case 0:
+            return "Ноль";
+        case 1:
+            return "Один";
+        case 2:
+            return "Два";
+        case 3:
+            return "Три";
+        case 4:
+            return "Четыре";
+        case 5:
+            return "Пять";
+        case 6:
+            return "Шесть";
+        case 7:
+            return "Семь";
+        case 8:
+            return "Восемь";
+        case 9:
+            return "Девять";
+        default:
+            return "Вы не ввели ни одну цифру из диапазона [0, 9]";
+    }
+}
+answer = testSwitchByNum(10)
+console.log(answer)
