dc4308f3508618dd3aaa301c1c42bc5fcafa27ff
GeekNekoS <anastasiyanesterenko@icloud.com>
2024-02-01 17:26:10 +0500

    Studied a minimum of JavaScript, started learning Java to write your mods for Minecraft

--- README.md
+++ README.md
@@ -4,17 +4,31 @@
 
 
 ## Загрузка и настройка готового проекта
-• Версия Python: 3.11.7
-
-• После скачивания проекта к себе на компьютер не забудьте установить необходимые зависимости, прописав к консоли команду: 
+ - Версия Python: 3.11.7
+ - После скачивания проекта к себе на компьютер не забудьте установить необходимые зависимости, прописав к консоли команду: 
 <code>pip install requirements.txt</code>
 <br /> <br />
 
 
-## Изучить
-⋄ Java Script ⋄ JQuery ⋄ Ajax 
+## IDEs
+ - Начало `PyCharm` проекта в папке `Nekos_study`: <br />
+java_script, machine_learning, mini_scripts, pdf_parsing
+<br /> <br />
+ - Начало `IntelliJ IDEA` проекта в папке `java`: <br />
+java
+<br /> <br />
+
+
+## Изучить / повторить
+⋄ Java ⋄ Java Script ⋄ JQuery ⋄ Ajax 
 
 ⋄ Machine learning
 
 ⋄ Flask ⋄ Django
 <br /> <br />
+
+
+## Полезные команды
+- Проверка версии: `javac -version`, `python --version`
+<br /> <br />
+- 
--- java_script/strings.js
+++ java_script/strings.js
@@ -17,8 +17,37 @@ catReversed = cat.charAt(2) + cat.charAt(1) + cat.charAt(0);
 // console.log(catReversed);
 
 
-function testStr(str, n) {
+function testLetterInStr(str, n) {
     return str[n-1];
 }
-// answer = testStr("Ithardlycousinmealways", 19)
+// answer = testLetterInStr("Ithardlycousinmealways", 19)
 // console.log(answer)
+
+
+const a = "первая строка";
+const b = " вторая строка";
+const c = " третья строка";
+const d = " четвертая строка";
+// result = a.concat(b , c, d);
+// console.log(result)
+
+
+const myString = "cat,dog,bird";
+// const myArray = myString.split(",");
+// console.log(myArray)
+
+
+const myString2 = "cat:dog:bird";
+// const myArray = myString2.split(":", 2);
+// console.log(myArray)
+
+
+function testStr(a, b) {
+    return a.toUpperCase() + b.toLowerCase();
+}
+answer = testStr('Hello', 'World!')
+console.log(answer)
+
+
+const myString3 = "fjkghskdhflkv"
+// answer = myString3.substring(indexA[, indexB])  // <==
